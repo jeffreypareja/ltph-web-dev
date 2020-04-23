52501b515a5043194c772f7d1ea9565cd816a836
Jeffrey Pareja <jeffrey@unosoft.ph>
2020-04-23 11:30:44 +0800

    description

--- site-config.js
+++ site-config.js
@@ -1,31 +1,32 @@
 module.exports = {
   // App Info
-  appstore_link         : "https://unosoftlabs.com",                                  // Enter App Store URL.
-  playstore_link        : "https://unosoftlabs.com",                                  // Enter Google Play Store URL.
-  google_analytics_ID   : "",                                        // Enter Google Analytics ID or ""
-  presskit_download_link: null,                                  // Enter a link to downloadable file or (e.g. public Dropbox link to a .zip file). 
-  video_or_screenshot   : "screenshot",                                           // "screenshot" or "video"
-  app_url               : "https://unosoftlabs.com",                            // Domain of your website without path_prefix.
-  path_prefix           : "/",                                                    // Prefixes all links. For cases when deployed to example.github.io/automatic-gatsbyjs-app-landing-page/.
-  app_name              : "Little Things PH",
-  app_price             : "Free",
-  app_description       : "Loren Ipsum dolor sit amet",
-  app_keywords          : ["comics", "little-things", "memes", "artworks"],
+  appstore_link: "https://unosoftlabs.com", // Enter App Store URL.
+  playstore_link: "https://unosoftlabs.com", // Enter Google Play Store URL.
+  google_analytics_ID: "", // Enter Google Analytics ID or ""
+  presskit_download_link: null, // Enter a link to downloadable file or (e.g. public Dropbox link to a .zip file).
+  video_or_screenshot: "screenshot", // "screenshot" or "video"
+  app_url: "https://unosoftlabs.com", // Domain of your website without path_prefix.
+  path_prefix: "/", // Prefixes all links. For cases when deployed to example.github.io/automatic-gatsbyjs-app-landing-page/.
+  app_name: "Little Things PH",
+  app_price: "Free",
+  app_description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+  app_keywords: ["comics", "little-things", "memes", "artworks"],
 
   // Personal Info
-  your_name              : "Unosoft Labs",
-  your_link              : "https://unosoftlabs.com",
-  your_city              : "Manila",
-  email_address          : null,
-  linkedin_username      : null,
-  facebook_username      : null,
-  instagram_username     : null,
-  twitter_username       : null,
-  github_username        : null,
-  youtube_username       : null,
+  your_name: "Unosoft Labs",
+  your_link: "https://unosoftlabs.com",
+  your_city: "Manila",
+  email_address: null,
+  linkedin_username: null,
+  facebook_username: null,
+  instagram_username: null,
+  twitter_username: null,
+  github_username: null,
+  youtube_username: null,
 
   // Features List
-  features               : [
+  features: [
     {
       title: "Read News",
       description:
@@ -51,23 +52,23 @@ module.exports = {
       fontawesome_icon_name: "play-circle",
     },
   ],
-  header_background             : "white",
-  topbar_title_color            : "black",
-  cover_overlay_color_rgba      : "rgba(54, 59, 61, 0.8)",
-  device_color                  : "black",                          // Set to "black", "blue", "coral", "white", or "yellow"
-  body_background_color         : "white",
-  primary_text_color            : "#000",
-  content_width                 : "1170px",
-  font                          : `"Cabin", sans-serif`,
-  link_color                    : "#1d63ea",
-  app_title_color               : "#f9591c",
-  app_price_color               : "#707070",
-  app_description_color         : "black",
-  feature_title_color           : "#f9591c",
-  feature_text_color            : "#666666",
+  header_background: "white",
+  topbar_title_color: "black",
+  cover_overlay_color_rgba: "rgba(54, 59, 61, 0.8)",
+  device_color: "black", // Set to "black", "blue", "coral", "white", or "yellow"
+  body_background_color: "white",
+  primary_text_color: "#000",
+  content_width: "1170px",
+  font: `"Cabin", sans-serif`,
+  link_color: "#1d63ea",
+  app_title_color: "#f9591c",
+  app_price_color: "#707070",
+  app_description_color: "black",
+  feature_title_color: "#f9591c",
+  feature_text_color: "#666666",
   feature_icons_foreground_color: "##f9591c",
   feature_icons_background_color: "#e6e6e6",
-  social_icons_foreground_color : "#666666",
-  social_icons_background_color : "#e6e6e6",
-  footer_text_color             : "#666666",
+  social_icons_foreground_color: "#666666",
+  social_icons_background_color: "#e6e6e6",
+  footer_text_color: "#666666",
 }
